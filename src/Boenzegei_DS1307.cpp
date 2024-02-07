23fe1d223294653b73d22170295d3da57bd39db5
Bonezegei <93502321+bonezegei@users.noreply.github.com>
2024-02-07 11:30:28 +0800

    Update Boenzegei_DS1307.cpp

--- src/Boenzegei_DS1307.cpp
+++ src/Boenzegei_DS1307.cpp
@@ -2,30 +2,63 @@
   This Library is written for DS1307 RTC
   Author: Bonezegei (Jofel Batutay)
   Date: Jan 28 2024
+  Updated: Feb 2024
 */
 
 #include "Bonezegei_DS1307.h"
 
-Bonezegei_DS1307::Bonezegei_DS1307(){}
-Bonezegei_DS1307::Bonezegei_DS1307(uint8_t addr){}
+Bonezegei_DS1307::Bonezegei_DS1307() {}
+Bonezegei_DS1307::Bonezegei_DS1307(uint8_t addr) {}
 
-char Bonezegei_DS1307::begin(){}
-uint8_t Bonezegei_DS1307::convert(uint8_t data){}
-uint8_t Bonezegei_DS1307::convertBCD(int data){}
-char Bonezegei_DS1307::getTime(){}
+char Bonezegei_DS1307::begin() {
+  return 0;
+}
+uint8_t Bonezegei_DS1307::convert(uint8_t data) {
+  return 0;
+}
+uint8_t Bonezegei_DS1307::convertBCD(int data) {
+  return 0;
+}
+char Bonezegei_DS1307::getTime() {
+  return 0;
+}
 
-uint8_t Bonezegei_DS1307::getSeconds(){}
-uint8_t Bonezegei_DS1307::getMinute(){}
-uint8_t Bonezegei_DS1307::getHour(){}
-uint8_t Bonezegei_DS1307::getDay();      //return Day of Week
-uint8_t Bonezegei_DS1307::getDate();     //return Date
-uint8_t Bonezegei_DS1307::getMonth();    //return Month
-uint8_t Bonezegei_DS1307::getYear();     //return Year
-uint8_t Bonezegei_DS1307::getAMPM();     //return 0=AM  1=PM
-uint8_t Bonezegei_DS1307::getFormat();   //return 12 or 24 hour format
+uint8_t Bonezegei_DS1307::getSeconds() {
+  return 0;
+}
+uint8_t Bonezegei_DS1307::getMinute() {
+  return 0;
+}
+uint8_t Bonezegei_DS1307::getHour() {
+  return 0;
+}
+uint8_t Bonezegei_DS1307::getDay() {
+  return 0;
+}
+uint8_t Bonezegei_DS1307::getDate() {
+  return 0;
+}
+uint8_t Bonezegei_DS1307::getMonth() {
+  return 0;
+}
+uint8_t Bonezegei_DS1307::getYear() {
+  return 0;
+}
+uint8_t Bonezegei_DS1307::getAMPM() {
+  return 0;
+}
+uint8_t Bonezegei_DS1307::getFormat() {
+  return 0;
+}
 
-void Bonezegei_DS1307::setFormat(uint8_t fmt);  // set time format 12 or 24
-void Bonezegei_DS1307::setAMPM(uint8_t ampm);   // set AM or PM   PM=1 AM=0
-void Bonezegei_DS1307::setTime(const char *t);  // set time Hour:Minute:Seconds
-void Bonezegei_DS1307::setDate(const char *d);  // set Date Month:Date:Year
-void Bonezegei_DS1307::setDay(uint8_t d);       // set Day of week
+void Bonezegei_DS1307::setFormat(uint8_t fmt) {
+}
+
+void Bonezegei_DS1307::setAMPM(uint8_t ampm) {
+}
+void Bonezegei_DS1307::setTime(const char *t) {
+}
+void Bonezegei_DS1307::setDate(const char *d) {
+}
+void Bonezegei_DS1307::setDay(uint8_t d) {
+}
